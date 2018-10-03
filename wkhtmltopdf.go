c7f4d2fe63368d0c00fd8f868cf555baf8a894c3
Joey Clover <joey.clover@gmail.com>
2018-10-03 10:00:48 +0100

    remove logs

--- wkhtmltopdf.go
+++ wkhtmltopdf.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -268,17 +267,10 @@ func (pdfg *PDFGenerator) Create() error {
 func (pdfg *PDFGenerator) run() error {
 
 	errbuf := &bytes.Buffer{}
-
-	log.Println("about to exec")
-
 	cmd := exec.Command(pdfg.binPath, pdfg.Args()...)
-
-	log.Println("command exec created")
-
 	cmd.Stdout = &pdfg.outbuf
 	cmd.Stderr = errbuf
 
-	log.Println("About to go through pages...")
 	//if there is a pageReader page (from Stdin) we set Stdin to that reader
 	for _, page := range pdfg.pages {
 		if page.Reader() != nil {
@@ -286,13 +278,8 @@ func (pdfg *PDFGenerator) run() error {
 			break
 		}
 	}
-	log.Println("Been through pages")
 
 	err := cmd.Run()
-
-	if err != nil {
-		log.Println("Got error... ", err)
-	}
 
 	if err != nil {
 		errStr := errbuf.String()
